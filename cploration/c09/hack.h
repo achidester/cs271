6c91111be144ac0a1f1aae6b22016e69bcbf6831
Achid2020 <55926511+Achid2020@users.noreply.github.com>
2021-12-09 11:51:11 -0800

    c09 exercise 1 complete.

---
+++ cploration/c09/hack.h
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdint.h>
+
+#define NUM_PREDEFINED_SYMBOLS 23
+
+enum symbol_id{
+  SYM_R0 = 0,
+  SYM_R1 = 1,
+  SYM_R2 = 2,
+  SYM_R3 = 3,
+  SYM_R4 = 4,
+  SYM_R5 = 5,
+  SYM_R6 = 6,
+  SYM_R7 = 7,
+  SYM_R8 = 8,
+  SYM_R9 = 9,
+  SYM_R10 = 10,
+  SYM_R11 = 11,
+  SYM_R12 = 12,
+  SYM_R13 = 13,
+  SYM_R14 = 14,
+  SYM_R15 = 15,
+  SYM_SP = 0,
+  SYM_LCL = 1,
+  SYM_ARG = 2,
+  SYM_THIS = 3,
+  SYM_THAT = 4,
+  SYM_SCREEN = 16384,
+  SYM_KBD = 24576,
+};
+
+struct 
